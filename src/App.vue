9df71faa5769a0ec5983f55944bf1fde801f4669
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-03-24 08:30:05 +0200

    initialize-project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <user-profile />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import userProfile from "./components/userProfile.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { userProfile },
+  name: "App",
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
---
+++ src/components/tweets.vue
@@ -0,0 +1,17 @@
+<template>
+  <span>
+    {{ content }}
+  </span>
+</template>
+
+<script>
+export default {
+  name: "tweets",
+  props: {
+    content: String,
+  },
+};
+</script>
+
+<style>
+</style>
